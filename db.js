1dc58422cdf434691245bb00e7de80ad298ccdcf
Laxmi Bansal <laxmibansal346@gmail.com>
2026-06-27 22:23:10 +0530

    fix: use connection pool to prevent timeout

--- db.js
+++ db.js
@@ -3,19 +3,24 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST?.trim(),
     user: process.env.DB_USER?.trim(),
     password: process.env.DB_PASSWORD?.trim(),
-    database: process.env.DB_NAME?.trim()
+    database: process.env.DB_NAME?.trim(),
+    port: process.env.DB_PORT?.trim(),
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if(err) {
         console.log('Database connection failed:', err.message);
         return;
     }
     console.log('Database connected successfully!');
+    connection.release();
 });
 
 module.exports = db;
